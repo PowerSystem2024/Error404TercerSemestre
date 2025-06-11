c0e8ab377173091173e412c2d4da5b61089de1de
Sebastian Diaz <sebas.dbz19@gmail.com>
2025-06-11 00:07:45 -0300

    Clase 9 - Java

--- Tecnicatura/AreaJuegos/AvatarLeyendaAang/js/avatar.js
+++ Tecnicatura/AreaJuegos/AvatarLeyendaAang/js/avatar.js
@@ -1,49 +1,95 @@
-// Seleccionamos los elementos del DOM
-const btnPersonajeJugador = document.getElementById("btn-personaje");
-// const btnFuego = document.getElementById("btn-fuego");
-// const btnAgua = document.getElementById("btn-agua");
-// const btnTierra = document.getElementById("btn-tierra");
-// const btnAire = document.getElementById("btn-aire");
-// const btnReiniciar = document.getElementById("btn-reiniciar");
+let ataqueJugador
+let ataqueEnemigo
 
-// Agregamos el evento al botón una vez cargado el DOM
-document.addEventListener("DOMContentLoaded", function () {
-  btnPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
-});
+function iniciarJuego(){
+    let botonPersonajeJugador = document.getElementById('boton-personaje');
+    botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
 
-// Función para elegir aleatoriamente el personaje del enemigo
-function aleatoria() {
-  const personajes = ["Zuko", "Katara", "Toph", "Aang"];
-  const indiceAleatorio = Math.floor(Math.random() * personajes.length);
-  return personajes[indiceAleatorio];
+    let botonPunio = document.getElementById('boton-punio') 
+    botonPunio.addEventListener('click', ataquePunio)
+    let botonPatada = document.getElementById('boton-patada')
+    botonPatada.addEventListener('click', ataquePatada)
+    let botonBarrida = document.getElementById('boton-barrida')
+    botonBarrida.addEventListener('click', ataqueBarrida)
 }
 
-// Función para seleccionar el personaje del jugador
-function seleccionarPersonajeJugador() {
-  const personajes = document.getElementsByName("personaje");
-  let personajeSeleccionado = null;
+function seleccionarPersonajeJugador(){
+  let inputZuko = document.getElementById('zuko')
+  let inputKatara = document.getElementById('katara')
+  let inputAang = document.getElementById('aang')
+  let inputToph = document.getElementById('toph')
 
-  // Buscar cuál personaje fue marcado
-  for (let i = 0; i < personajes.length; i++) {
-    if (personajes[i].checked) {
-      personajeSeleccionado = personajes[i];
-      break;
-    }
-  }
+  let spanPersonajeJugador = document.getElementById('personaje-jugador')
 
-  // Si se eligió un personaje
-  if (personajeSeleccionado) {
-    const nombreJugador = personajeSeleccionado.labels[0].innerText;
-    const nombreEnemigo = aleatoria();
-
-    // Mostrar alertas
-    alert(`SELECCIONASTE TU PERSONAJE: ${nombreJugador.toUpperCase()}`);
-    alert(`LA PC HA ELEGIDO EL PERSONAJE: ${nombreEnemigo.toUpperCase()}`);
-
-    // Actualizar nombres en la sección de ataque
-    document.getElementById("personaje-jugador").innerText = nombreJugador;
-    document.getElementById("personaje-enemigo").innerText = nombreEnemigo;
+  if(inputZuko.checked){
+      spanPersonajeJugador.innerHTML = 'Zuko'
+      alert('SELECCIONASTE TU PERSONAJE: ZUKO')
+  } else if(inputKatara.checked){
+      spanPersonajeJugador.innerHTML = 'Katara'
+      alert('SELECCIONASTE TU PERSONAJE: KATARA')
+  } else if(inputAang.checked){
+      spanPersonajeJugador.innerHTML = 'Aang'
+      alert('SELECCIONASTE TU PERSONAJE: AANG')
+  } else if(inputToph.checked){
+      spanPersonajeJugador.innerHTML = 'Toph'
+      alert('SELECCIONASTE TU PERSONAJE: TOPH')
   } else {
-    alert("No has seleccionado ningún personaje.");
+      alert('Selecciona un personaje')
+      return
   }
+  
+  seleccionarPersonajeEnemigo();
 }
+
+
+function seleccionarPersonajeEnemigo() {
+  let personajes = ['Zuko', 'Katara', 'Aang', 'Toph'];
+  let indiceAleatorio = Math.floor(Math.random() * personajes.length);
+
+  let spanPersonajeEnemigo = document.getElementById('personaje-enemigo');
+  let personajeEnemigo = personajes[indiceAleatorio];
+  spanPersonajeEnemigo.innerHTML = personajeEnemigo;
+
+  alert(`LA PC HA ELEGIDO EL PERSONAJE: ${personajeEnemigo.toUpperCase()}`);
+}
+
+
+function ataquePunio(){ 
+    ataqueJugador = 'Punio'
+    ataqueAleatorioEnemigo()
+}
+
+function ataquePatada(){ 
+    ataqueJugador = 'Patada'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueBarrida(){ 
+    ataqueJugador = 'Barrida'
+    ataqueAleatorioEnemigo()
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1, 3)
+
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = 'Punio'
+    } else if(ataqueAleatorio == 2){
+        ataqueEnemigo = 'Patada'
+    } else {
+        ataqueEnemigo = 'Barrida'
+    }
+
+    mostrarAtaques();
+}
+
+function mostrarAtaques() {
+    const mensajes = document.querySelector('#mensajes p');
+    mensajes.innerHTML += `<br>Tu personaje lanzó ${ataqueJugador} y el enemigo lanzó ${ataqueEnemigo}.`;
+}
+
+window.addEventListener('load', iniciarJuego)
